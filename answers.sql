45157f57ba6190ceb3703a1d34d24f6aab9c2972
Ian Wandago <winstonwandago@gmail.com>
2025-09-06 11:22:23 +0300

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,17 @@
+USE salesdb;
+-- Q1. Drop the index named IdxPhone from the customers table:
+DROP INDEX IdxPhone ON customers;
+
+-- Q2. DROP INDEX IdxPhone ON customers;
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- Q3. CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+-- Q4. GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
+
+
+
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
